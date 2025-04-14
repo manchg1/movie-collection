010f899c3f1db173a03969322455577e9d0c9347
Gaurav Manchanda <gaurav.manchanda@nokia.com>
2025-04-14 14:59:35 -0400

    Add edit route page.js to fetch and render EditForm

---
+++ src/app/admin/edit/[id]/page.js
@@ -0,0 +1,17 @@
+import EditForm from './EditForm';
+
+export default async function EditMoviePage({ params }) {
+    const res = await fetch(`http://localhost:4000/movies/${params.id}`);
+    const movie = await res.json();
+
+    if (!movie.id) {
+        return <p className="text-red-600 p-4">Movie not found.</p>;
+    }
+
+    return (
+        <main className="p-6">
+            <h1 className="text-xl font-bold mb-4">Edit Movie</h1>
+            <EditForm movie={movie} />
+        </main>
+    );
+}
